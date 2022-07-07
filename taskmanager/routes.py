9aa63fb04ba554f8a3cad39d80cbf4e009b763b2
chloë js <chloejs.horta@gmail.com>
2022-07-07 16:32:14 +0000

    include db

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,14 +1,8 @@
 from flask import render_template
 from taskmanager import app, db
+from taskmanager.models import Category, Task
 
 
 @app.route("/")
 def home():
     return render_template("base.html")
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-
-    Terms
-    Privacy
-    Secur
